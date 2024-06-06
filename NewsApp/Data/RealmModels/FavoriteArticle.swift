ea965d021cff6b6d30bceb3fee223f0689e03242
Ahmad Aboelghet <agmadaboelghet@gmail.com>
2024-06-06 18:56:18 +0300

    Realm models commit

--- NewsApp/Data/RealmModels/FavoriteArticle.swift
+++ NewsApp/Data/RealmModels/FavoriteArticle.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import RealmSwift
+
+class FavoriteArticle: Object {
+    @objc dynamic var title = ""
+    @objc dynamic var date = ""
+    @objc dynamic var imageUrl = ""
+    @objc dynamic var source = ""
+    @objc dynamic var descriptionText = ""
+
+    convenience init(article: Article) {
+        self.init()
+        self.title = article.title
+        self.date = article.publishedAt
+        self.imageUrl = article.urlToImage ?? ""
+        self.source = article.source.name
+        self.descriptionText = article.description
+    }
+
+    func toArticle() -> Article {
+        return Article(title: title, publishedAt: date, urlToImage: imageUrl, source: Source(name: source), description: descriptionText)
+    }
+}
